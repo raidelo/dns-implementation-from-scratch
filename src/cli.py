1bed62f14df5712d7c08c84b01efeedb4aed3b8a
raidelo <raidelosix@gmail.com>
2025-09-27 16:55:35 -0400

    feat: add argument to the program to specify the query class

--- src/cli.py
+++ src/cli.py
@@ -6,6 +6,7 @@ def parse_args():
     parser.add_argument("domains", nargs="+")
     parser.add_argument("-s", "--server", default="8.8.8.8:53", dest="server")
     parser.add_argument("-t", "--qtype", default="A", dest="qtype")
+    parser.add_argument("-c", "--qclass", default="IN", dest="qclass")
     parser.add_argument(
         "-n",
         "--non-recursive",
